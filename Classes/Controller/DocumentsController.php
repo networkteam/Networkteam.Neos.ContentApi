67bea7a9f6b189e7841c959173de0c4c3e0618f1
Christopher Hlubek <christopher.hlubek@networkteam.com>
2024-03-26 17:11:07 +0100

    feat: Add support for RedirectHandler

--- Classes/Controller/DocumentsController.php
+++ Classes/Controller/DocumentsController.php
@@ -12,6 +12,7 @@ use Neos\Flow\Mvc\Controller\ActionController;
 use Neos\Flow\Mvc\Routing\Dto\RouteParameters;
 use Neos\Flow\Mvc\Routing\Exception\MissingActionNameException;
 use Neos\Flow\Mvc\View\JsonView;
+use Neos\Flow\Utility\Now;
 use Neos\Neos\Domain\Repository\DomainRepository;
 use Neos\Neos\Domain\Repository\SiteRepository;
 use Neos\Neos\Domain\Service\ContentContextFactory;
@@ -25,243 +26,304 @@ use Networkteam\Neos\ContentApi\Http\DimensionsHelper;
 class DocumentsController extends ActionController
 {
 
-    use ErrorHandlingTrait;
-    use SiteHandlingTrait;
+	use ErrorHandlingTrait;
+	use SiteHandlingTrait;
 
-    /**
-     * @var string
-     */
-    protected $defaultViewObjectName = JsonView::class;
+	/**
+	 * @var string
+	 */
+	protected $defaultViewObjectName = JsonView::class;
 
-    /**
-     * @Flow\Inject
-     * @var NodeEnumerator
-     */
-    protected $nodeEnumerator;
+	/**
+	 * @Flow\Inject
+	 * @var NodeEnumerator
+	 */
+	protected $nodeEnumerator;
 
-    /**
-     * @Flow\Inject
-     * @var ContentContextFactory
-     */
-    protected $contentContextFactory;
+	/**
+	 * @Flow\Inject
+	 * @var ContentContextFactory
+	 */
+	protected $contentContextFactory;
 
-    /**
-     * @Flow\Inject
-     * @var DomainRepository
-     */
-    protected $domainRepository;
+	/**
+	 * @Flow\Inject
+	 * @var DomainRepository
+	 */
+	protected $domainRepository;
 
-    /**
-     * @var ContentDimensionPresetSourceInterface
-     * @Flow\Inject
-     */
-    protected $contentDimensionPresetSource;
+	/**
+	 * @var ContentDimensionPresetSourceInterface
+	 * @Flow\Inject
+	 */
+	protected $contentDimensionPresetSource;
 
-    /**
-     * @Flow\Inject
-     * @var SiteRepository
-     */
-    protected $siteRepository;
+	/**
+	 * @Flow\Inject
+	 * @var SiteRepository
+	 */
+	protected $siteRepository;
 
-    /**
-     * @Flow\InjectConfiguration(path="documentList.ignoredNodeTypes")
-     * @var array
-     */
-    protected $ignoredNodeTypes = [];
+	/**
+	 * @Flow\Inject
+	 * @var Now
+	 */
+	protected $now;
 
-    /**
-     * List all document nodes
-     *
-     * @param string $workspaceName Workspace name for node context (defaults to live)
-     *
-     * @throws NodeException
-     * @throws MissingActionNameException
-     * @throws \Neos\Flow\Http\Exception
-     * @throws \Neos\Neos\Domain\Exception
-     */
-    public function indexAction(string $workspaceName = 'live'): void
-    {
-        // TODO Check that public access is only granted to live workspace (or content api is completely restricted by API key)
+	/**
+	 * @Flow\InjectConfiguration(path="documentList.ignoredNodeTypes")
+	 * @var array
+	 */
+	protected $ignoredNodeTypes = [];
 
-        $documents = [];
-        $availableDimensions = [];
+	/**
+	 * @Flow\InjectConfiguration(path="checkRedirects")
+	 * @var boolean
+	 */
+	protected $checkRedirects = false;
+
+	/**
+	 * List all document nodes
+	 *
+	 * @param string $workspaceName Workspace name for node context (defaults to live)
+	 *
+	 * @throws NodeException
+	 * @throws MissingActionNameException
+	 * @throws \Neos\Flow\Http\Exception
+	 * @throws \Neos\Neos\Domain\Exception
+	 */
+	public function indexAction(string $workspaceName = 'live'): void
+	{
+		// TODO Check that public access is only granted to live workspace (or content api is completely restricted by API key)
+
+		$documents = [];
+		$availableDimensions = [];
 
 		// Make sure to check for dimension values from routing parameters to support Flowpack.Neos.DimensionResolver
 		$dimensionValues = DimensionsHelper::getDimensionValuesFromRequest($this->request->getHttpRequest());
 
-        $site = $this->getActiveSite();
-        $siteNodeName = $site->getNodeName();
-        foreach ($this->nodeEnumerator->siteNodeInContexts($site, $workspaceName, $dimensionValues) as $siteNode) {
-            foreach ($this->nodeEnumerator->recurseDocumentChildNodes($siteNode) as $documentNode) {
-                $nodeType = $documentNode->getNodeType();
-                if ($this->isIgnoredNodeType($nodeType)) {
-                    continue;
-                }
+		$site = $this->getActiveSite();
+		$siteNodeName = $site->getNodeName();
+		foreach ($this->nodeEnumerator->siteNodeInContexts($site, $workspaceName, $dimensionValues) as $siteNode) {
+			foreach ($this->nodeEnumerator->recurseDocumentChildNodes($siteNode) as $documentNode) {
+				$nodeType = $documentNode->getNodeType();
+				if ($this->isIgnoredNodeType($nodeType)) {
+					continue;
+				}
 
-                $nodeAggregateIdentifier = $documentNode->getNodeAggregateIdentifier();
-                $creationDateTime = $documentNode->getCreationDateTime();
-                $lastPublicationDateTime = $documentNode->getLastPublicationDateTime();
-                $availableDimensions = $this->contentDimensionPresetSource->getAllPresets();
-                $dimensions = $documentNode->getContext()->getDimensions();
-                $routePath = $this->uriBuilder->uriFor(
-                    'show',
-                    [
-                        'node' => $documentNode,
-                    ],
-                    'Frontend\Node',
-                    'Neos.Neos',
-                );
-                $documents[] = [
-                    'identifier' => (string)$nodeAggregateIdentifier,
-                    'contextPath' => $documentNode->getContextPath(),
-                    'dimensions' => $dimensions,
-                    'site' => $siteNodeName,
-                    'routePath' => $routePath,
-                    'creationDateTime' => $creationDateTime,
-                    'lastPublicationDateTime' => $lastPublicationDateTime
-                ];
-            }
-        }
+				$nodeAggregateIdentifier = $documentNode->getNodeAggregateIdentifier();
+				$creationDateTime = $documentNode->getCreationDateTime();
+				$lastPublicationDateTime = $documentNode->getLastPublicationDateTime();
+				$availableDimensions = $this->contentDimensionPresetSource->getAllPresets();
+				$dimensions = $documentNode->getContext()->getDimensions();
+				$routePath = $this->uriBuilder->uriFor(
+					'show',
+					[
+						'node' => $documentNode,
+					],
+					'Frontend\Node',
+					'Neos.Neos',
+				);
+				$documents[] = [
+					'identifier' => (string)$nodeAggregateIdentifier,
+					'contextPath' => $documentNode->getContextPath(),
+					'dimensions' => $dimensions,
+					'site' => $siteNodeName,
+					'routePath' => $routePath,
+					'creationDateTime' => $creationDateTime,
+					'lastPublicationDateTime' => $lastPublicationDateTime
+				];
+			}
+		}
 
-        $this->view->assign('value', [
-            'documents' => $documents,
-            'dimensions' => $availableDimensions,
-        ]);
-    }
+		$this->view->assign('value', [
+			'documents' => $documents,
+			'dimensions' => $availableDimensions,
+		]);
+	}
 
-    /**
-     * Render properties of a document node for the content API
-     *
-     * Either provide a path (public frontend) or context path (preview / editing) to fetch the node.
-     *
-     * @param string|null $path Node route path (e.g. "/en/features")
-     * @param string|null $contextPath Node context path (e.g. "/sites/neosdemo@user-admin;language=en_US")
-     *
-     * @throws Exception\NodeNotFoundException
-     * @throws \Neos\Flow\Mvc\Exception
-     */
-    public function documentAction(?string $path = null, ?string $contextPath = null): void
-    {
-        if ($path !== null) {
-            $path = ltrim($path, '/');
+	/**
+	 * Render properties of a document node for the content API
+	 *
+	 * Either provide a path (public frontend) or context path (preview / editing) to fetch the node.
+	 *
+	 * @param string|null $path Node route path (e.g. "/en/features")
+	 * @param string|null $contextPath Node context path (e.g. "/sites/neosdemo@user-admin;language=en_US")
+	 *
+	 * @throws Exception\NodeNotFoundException
+	 * @throws \Neos\Flow\Mvc\Exception
+	 */
+	public function documentAction(?string $path = null, ?string $contextPath = null): void
+	{
+		if ($path !== null) {
+			$path = ltrim($path, '/');
 
-            $routePart = $this->objectManager->get(FrontendNodeRoutePartHandlerInterface::class);
-            $routePart->setName('node');
+			$routePart = $this->objectManager->get(FrontendNodeRoutePartHandlerInterface::class);
+			$routePart->setName('node');
 
-            $parameters = $this->request->getHttpRequest()->getAttribute(ServerRequestAttributes::ROUTING_PARAMETERS) ?? RouteParameters::createEmpty();
-            $matchResult = $routePart->matchWithParameters($path, $parameters);
-            if ($matchResult === false) {
-                throw new Exception\NodeNotFoundException(sprintf('Node with path %s not found', $path), 1611250322);
-            }
+			$parameters = $this->request->getHttpRequest()->getAttribute(ServerRequestAttributes::ROUTING_PARAMETERS) ?? RouteParameters::createEmpty();
+			$matchResult = $routePart->matchWithParameters($path, $parameters);
+			if ($matchResult === false) {
+				$redirect = $this->findPossibleRedirect($path, $this->request->getHttpRequest()->getUri()->getHost());
+				if ($redirect !== null) {
+					$this->view->assign('value', [
+						'redirect' => $redirect
+					]);
+					return;
+				}
 
-            $nodeContextPath = $matchResult === true ? $routePart->getValue() : $matchResult->getMatchedValue();
-        } elseif ($contextPath !== null) {
-            $nodeContextPath = $contextPath;
-        } else {
-            throw new Exception\NodeNotFoundException('No path or context path provided', 1662111662);
-        }
+				throw new Exception\NodeNotFoundException(sprintf('Node with path %s not found', $path), 1611250322);
+			}
 
-        $nodePathAndContext = NodePaths::explodeContextPath($nodeContextPath);
-        $nodePath = $nodePathAndContext['nodePath'];
-        $workspaceName = $nodePathAndContext['workspaceName'];
-        $dimensions = $nodePathAndContext['dimensions'];
+			$nodeContextPath = $matchResult === true ? $routePart->getValue() : $matchResult->getMatchedValue();
+		} elseif ($contextPath !== null) {
+			$nodeContextPath = $contextPath;
+		} else {
+			throw new Exception\NodeNotFoundException('No path or context path provided', 1662111662);
+		}
 
-        $contentContext = $this->contentContextFactory->create($this->prepareContextProperties($workspaceName,
-            $dimensions));
-        $documentNode = $contentContext->getNode($nodePath);
-        if (!$documentNode instanceof NodeInterface) {
-            throw new Exception\NodeNotFoundException(sprintf('Node with node path "%s" not found', $nodePath),
-                1611245114);
-        }
+		$nodePathAndContext = NodePaths::explodeContextPath($nodeContextPath);
+		$nodePath = $nodePathAndContext['nodePath'];
+		$workspaceName = $nodePathAndContext['workspaceName'];
+		$dimensions = $nodePathAndContext['dimensions'];
 
-        $viewOptions = [];
-        $fusionView = new FusionView($viewOptions);
-        // TODO Add custom Response and intercept headers from result
-        $fusionView->setControllerContext($this->controllerContext);
-        $fusionView->assign('site', $contentContext->getCurrentSiteNode());
-        $fusionView->assign('value', $documentNode);
-        $fusionView->setFusionPath('contentApi/document');
-        $result = $fusionView->render();
-        $this->view->assign('value', $result);
-    }
+		$contentContext = $this->contentContextFactory->create($this->prepareContextProperties($workspaceName,
+			$dimensions));
+		$documentNode = $contentContext->getNode($nodePath);
+		if (!$documentNode instanceof NodeInterface) {
+			if ($path !== null) {
+				$redirect = $this->findPossibleRedirect($path, $this->request->getHttpRequest()->getUri()->getHost());
+				if ($redirect !== null) {
+					$this->view->assign('value', [
+						'redirect' => $redirect
+					]);
+					return;
+				}
+			}
 
-    /**
-     * Render properties of a single node for the content API
-     *
-     * @param string $contextPath Node context path (e.g. "/sites/neosdemo@user-admin;language=en_US")
-     *
-     * @throws Exception\NodeNotFoundException
-     * @throws \Neos\Flow\Mvc\Exception
-     */
-    public function nodeAction(string $contextPath): void
-    {
-        $nodePathAndContext = NodePaths::explodeContextPath($contextPath);
-        $nodePath = $nodePathAndContext['nodePath'];
-        $workspaceName = $nodePathAndContext['workspaceName'];
-        $dimensions = $nodePathAndContext['dimensions'];
+			throw new Exception\NodeNotFoundException(sprintf('Node with node path "%s" not found', $nodePath),
+				1611245114);
+		}
 
-        $contentContext = $this->contentContextFactory->create($this->prepareContextProperties($workspaceName,
-            $dimensions));
-        $node = $contentContext->getNode($nodePath);
-        if (!$node instanceof NodeInterface) {
-            throw new Exception\NodeNotFoundException(sprintf('Node with path "%s" not found', $nodePath),
-                1611245114);
-        }
+		$viewOptions = [];
+		$fusionView = new FusionView($viewOptions);
+		// TODO Add custom Response and intercept headers from result
+		$fusionView->setControllerContext($this->controllerContext);
+		$fusionView->assign('site', $contentContext->getCurrentSiteNode());
+		$fusionView->assign('value', $documentNode);
+		$fusionView->setFusionPath('contentApi/document');
+		$result = $fusionView->render();
+		$this->view->assign('value', $result);
+	}
 
-        $viewOptions = [];
-        $fusionView = new FusionView($viewOptions);
-        // TODO Add custom Response and intercept headers from result
-        $fusionView->setControllerContext($this->controllerContext);
-        $fusionView->assign('site', $contentContext->getCurrentSiteNode());
-        $fusionView->assign('value', $node);
-        $fusionView->setFusionPath('contentApi/node');
-        $result = $fusionView->render();
-        $this->view->assign('value', $result);
-    }
+	/**
+	 * Render properties of a single node for the content API
+	 *
+	 * @param string $contextPath Node context path (e.g. "/sites/neosdemo@user-admin;language=en_US")
+	 *
+	 * @throws Exception\NodeNotFoundException
+	 * @throws \Neos\Flow\Mvc\Exception
+	 */
+	public function nodeAction(string $contextPath): void
+	{
+		$nodePathAndContext = NodePaths::explodeContextPath($contextPath);
+		$nodePath = $nodePathAndContext['nodePath'];
+		$workspaceName = $nodePathAndContext['workspaceName'];
+		$dimensions = $nodePathAndContext['dimensions'];
 
-    /**
-     * Prepares the context properties for the nodes based on the given workspace and dimensions
-     *
-     * @param string $workspaceName
-     * @param array $dimensions
-     * @return array
-     */
-    protected function prepareContextProperties(string $workspaceName, array $dimensions = null): array
-    {
-        $contextProperties = [
-            'workspaceName' => $workspaceName,
-            'invisibleContentShown' => $workspaceName !== 'live',
-            'inaccessibleContentShown' => $workspaceName !== 'live',
-            'removedContentShown' => false
-        ];
+		$contentContext = $this->contentContextFactory->create($this->prepareContextProperties($workspaceName,
+			$dimensions));
+		$node = $contentContext->getNode($nodePath);
+		if (!$node instanceof NodeInterface) {
+			throw new Exception\NodeNotFoundException(sprintf('Node with path "%s" not found', $nodePath),
+				1611245114);
+		}
 
-        if ($dimensions !== null) {
-            $contextProperties['dimensions'] = $dimensions;
-        }
+		$viewOptions = [];
+		$fusionView = new FusionView($viewOptions);
+		// TODO Add custom Response and intercept headers from result
+		$fusionView->setControllerContext($this->controllerContext);
+		$fusionView->assign('site', $contentContext->getCurrentSiteNode());
+		$fusionView->assign('value', $node);
+		$fusionView->setFusionPath('contentApi/node');
+		$result = $fusionView->render();
+		$this->view->assign('value', $result);
+	}
 
-        return $contextProperties;
-    }
+	/**
+	 * Prepares the context properties for the nodes based on the given workspace and dimensions
+	 *
+	 * @param string $workspaceName
+	 * @param array $dimensions
+	 * @return array
+	 */
+	protected function prepareContextProperties(string $workspaceName, array $dimensions = null): array
+	{
+		$contextProperties = [
+			'workspaceName' => $workspaceName,
+			'invisibleContentShown' => $workspaceName !== 'live',
+			'inaccessibleContentShown' => $workspaceName !== 'live',
+			'removedContentShown' => false
+		];
 
-    /**
-     * @return array
-     * @throws \Networkteam\Neos\ContentApi\Exception
-     */
-    private function getIgnoredNodeTypes(): array
-    {
-        $ignoredNodeTypes = $this->ignoredNodeTypes ?? [];
-        if (!is_array($ignoredNodeTypes)) {
-            throw new Exception('The "ignoredNodeTypes" setting must be an array of node type names', 1669719500);
-        }
-        return $ignoredNodeTypes;
-    }
+		if ($dimensions !== null) {
+			$contextProperties['dimensions'] = $dimensions;
+		}
 
-    private function isIgnoredNodeType(NodeType $nodeType): bool
-    {
-        foreach ($this->getIgnoredNodeTypes() as $ignoredNodeType) {
-            if ($nodeType->isOfType($ignoredNodeType)) {
-                return true;
-            }
-        }
-        return false;
-    }
+		return $contextProperties;
+	}
+
+	/**
+	 * @return array
+	 * @throws \Networkteam\Neos\ContentApi\Exception
+	 */
+	private function getIgnoredNodeTypes(): array
+	{
+		$ignoredNodeTypes = $this->ignoredNodeTypes ?? [];
+		if (!is_array($ignoredNodeTypes)) {
+			throw new Exception('The "ignoredNodeTypes" setting must be an array of node type names', 1669719500);
+		}
+		return $ignoredNodeTypes;
+	}
+
+	private function isIgnoredNodeType(NodeType $nodeType): bool
+	{
+		foreach ($this->getIgnoredNodeTypes() as $ignoredNodeType) {
+			if ($nodeType->isOfType($ignoredNodeType)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private function findPossibleRedirect(string $path, string $host): ?array
+	{
+		if (!$this->checkRedirects) {
+			return null;
+		}
+
+		// Check if RedirectHandler is available
+		if (!$this->objectManager->has('Neos\RedirectHandler\Storage\RedirectStorageInterface')) {
+			return null;
+		}
+
+		$redirectStorage = $this->objectManager->get('Neos\RedirectHandler\Storage\RedirectStorageInterface');
+		$redirect = $redirectStorage->getOneBySourceUriPathAndHost($path, $host);
+		if ($redirect === null) {
+			return null;
+		}
+
+		// Check if redirect ist still valid
+		if ($redirect->getStartDateTime() !== null && $redirect->getStartDateTime()->getTimestamp() > $this->now->getTimestamp()) {
+			return null;
+		}
+		if ($redirect->getEndDateTime() !== null && $redirect->getEndDateTime()->getTimestamp() <= $this->now->getTimestamp()) {
+			return null;
+		}
+
+		return [
+			'targetPath' => '/' . ltrim($redirect->getTargetUriPath(), '/'),
+			'statusCode' => $redirect->getStatusCode(),
+		];
+	}
 }
--- README.md
+++ README.md
@@ -6,22 +6,57 @@
 
 The content API will use the current domain / site of the request, so make sure to call the API via the correct domain when in a multi-site installation.
 
+### Extensibility via Fusion
+
+The API responses are declared by Fusion prototypes. This allows for a high degree of flexibility and customization.
+
+### Simple API
+
+This package does not offer a complex API for querying nodes.
+It is mainly focused on rendering pages backed by document nodes when using Neos as a headless CMS. It is also perfect to
+pair with [@networkteam/zebra](https://github.com/networkteam/zebra) and supports full editing of nodes with visual editing.
+
+A node can be fetched either by path (for public access) or by context path (for access with preview in workspaces).
+
+## Features
+
+* Support for multi-site installations
+* Supports dimensions (e.g. for multi-language sites)
+* Supports [Flowpack.Neos.DimensionResolver](https://github.com/Flowpack/neos-dimensionresolver) for flexible dimension routing
+* Supports Neos.RedirectHandler (if installed) and `checkRedirects` is enabled in settings
+
+## Configuration
+
+```yaml
+Networkteam:
+  Neos:
+    ContentApi:
+      recursiveReferencePropertyDepth: 1
+      documentList:
+        ignoredNodeTypes:
+          - 'Neos.Neos:Shortcut'
+      # Enable to check redirects of RedirectsHandler (if package is available) if a node is not found
+      checkRedirects: false
+```
+
 ## Examples
 
 ### Adding an API prototype for document node types:
 
-    prototype(Neos.Demo:Document.Page.Api) < prototype(Networkteam.Neos.ContentApi:DefaultDocument) {
-        content {
-            main = Neos.Neos:ContentCollection {
-                nodePath = 'main'
-            }
-            teaser = Neos.Neos:ContentCollection {
-                nodePath = 'teaser'
-            }
-        }
-    }
-    prototype(Neos.Demo:Document.LandingPage.Api) < prototype(Neos.Demo:Document.Page.Api)
-    prototype(Neos.Demo:Document.Homepage.Api) < prototype(Neos.Demo:Document.Page.Api)
+```neosfusion
+prototype(Neos.Demo:Document.Page.Api) < prototype(Networkteam.Neos.ContentApi:DefaultDocument) {
+	content {
+		main = Neos.Neos:ContentCollection {
+			nodePath = 'main'
+		}
+		teaser = Neos.Neos:ContentCollection {
+			nodePath = 'teaser'
+		}
+	}
+}
+prototype(Neos.Demo:Document.LandingPage.Api) < prototype(Neos.Demo:Document.Page.Api)
+prototype(Neos.Demo:Document.Homepage.Api) < prototype(Neos.Demo:Document.Page.Api)
+```
 
 The idea here is to have a `.Api` prototype for the complete document
 node type hierarchy.
@@ -30,46 +65,56 @@ serialized as JSON.
 
 ### Extending API site properties
 
-    contentApi {
-        site {
-            # Set some additional context variables for default Fusion to work correctly
-            @context {
-                documentNode = ${site}
-                node = ${site}
-            }
-            navigation = Neos.Fusion:DataStructure {
-                mainItems = Neos.Fusion:Map {
-                    items = ${q(site).children('[instanceof Neos.Neos:Document][_hiddenInIndex=false]')}
-                    itemName = 'node'
-                    itemRenderer = Neos.Fusion:DataStructure {
-                        title = ${q(node).property('title')}
-                        renderPath = Neos.Neos:NodeUri {
-                            node = ${node}
-                            format = 'html'
-                        }
-                    }
-                }
-            }
-            content {
-                footer = Neos.Neos:ContentCollection {
-                    nodePath = 'footer'
-                }
-            }
-        }
-    }
+```neosfusion
+contentApi {
+	site {
+		# Set some additional context variables for default Fusion to work correctly
+		@context {
+			documentNode = ${site}
+			node = ${site}
+		}
+		navigation = Neos.Fusion:DataStructure {
+			mainItems = Neos.Fusion:Map {
+				items = ${q(site).children('[instanceof Neos.Neos:Document][_hiddenInIndex=false]')}
+				itemName = 'node'
+				itemRenderer = Neos.Fusion:DataStructure {
+					title = ${q(node).property('title')}
+					renderPath = Neos.Neos:NodeUri {
+						node = ${node}
+						format = 'html'
+					}
+				}
+			}
+		}
+		content {
+			footer = Neos.Neos:ContentCollection {
+				nodePath = 'footer'
+			}
+		}
+	}
+}
+```
 
 This can be fetched via the `/content-api/site` endpoint for the current site (depends on domain).
 
 ## API endpoints
 
-### `/content-api/documents`
+### `/neos/content-api/documents`
 
-### `/content-api/document`
+Lists available documents with route path / context path and iterating through dimensions.
+
+A different workspace can be selected via `workspaceName` (needs authentication).
+
+> Note: If Flowpack.Neos.DimensionResolver already resolved dimensions e.g. based on the domain, then the dimensions are not iterated.
+
+### `/neos/content-api/document`
 
 Render a document given by `path` or `contextPath`.
 
-### `/content-api/node`
+### `/neos/content-api/node`
 
 Render a single node given by `contextPath`.
 
-### `/content-api/site`
+### `/neos/content-api/site`
+
+Render site properties independent of a single node.
